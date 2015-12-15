2614511d77ac35a254e2abec03c3378157a6a318
mzvast <mzvast@gmail.com>
2015-12-15 08:20:32 +0000

    CHANGE template engine to express-handlebars SEPARATE bootstrap into layouts and partials TEST jQuery works

--- wein.js
+++ wein.js
@@ -3,18 +3,20 @@ var express = require('express');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express3-handlebars').create({
-    defaultLayout:'main_bt',
-    helpers: {
-        section: function(name, options){
-            if(!this._sections) this._sections = {};
-            this._sections[name] = options.fn(this);
-            return null;
-        }
-    }
-});
-app.engine('handlebars', handlebars.engine);
+var exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main_bt',
+	helpers: {
+		section: function(name, options) {
+			if (!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	}
+}));
 app.set('view engine', 'handlebars');
+
 
 app.set('port', process.env.PORT || 8080);
 
@@ -31,30 +33,32 @@ var fortuneCookies = [
 app.get('/', function(req, res) {
 	res.render('home');
 });
-app.get('/about', function(req,res){
-	var randomFortune = 
+app.get('/about', function(req, res) {
+	var randomFortune =
 		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', {
+		fortune: randomFortune
+	});
 });
-app.get('/jquery', function(req, res){
+app.get('/jquery', function(req, res) {
 	res.render('jquery-test');
 });
 
 
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.status(404);
 	res.render('404');
 });
 
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' + 
-    app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function() {
+	console.log('Express started on http://localhost:' +
+		app.get('port') + '; press Ctrl-C to terminate.');
 });
