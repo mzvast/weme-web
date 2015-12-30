e355e552193c0947c63128d197e3b619f04a946e
mzvast <mzvast@gmail.com>
2015-12-30 13:36:17 +0800

    Test user login logic OK

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -1,10 +1,29 @@
-var ViewModel = function() {
-	var self = this;
-	self.username = ko.observable();
-	self.password = ko.observable();
-	self.doLogin=function() {
-		console.log(md5(self.password));
-	};
-};
+$(function() {
 
-ko.applyBindings(new ViewModel())
+	var ViewModel = function() {
+		var self = this;
+		self.username = ko.observable('');
+		self.password = ko.observable('');
+		self.passhash = md5(self.password());
+		self.doLogin=function() {
+			$.ajax({
+				  type: "POST",
+				  url: "/api/post/login",
+				  dataType: "json",
+				  data:{
+				  	"username": ""+self.username(),
+				  	"password": ""+self.passhash
+				  },
+				  success: function(json) {
+				       console.log("token: "+json['token']);
+				    },
+				  error: function(e) {
+				       console.log(e);
+    				}
+				});
+		};
+	};
+
+	ko.applyBindings(new ViewModel())
+	
+})
