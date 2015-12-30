a681bc3c1d9b73a08715b24c0a9d33c9b1d2e5f0
mzvast <mzvast@gmail.com>
2015-12-30 15:40:41 +0800

    Fix login router issue NOW can login to admin!

--- weme.js
+++ weme.js
@@ -3,9 +3,10 @@ var http = require('http');
 var querystring = require('querystring');
 var app = express();
 var router = express.Router();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var session = require('express-session');
 // set up handlebars view engine
-var exphbs = require('express-handlebars')
+var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main_bt',
 	helpers: {
@@ -26,18 +27,70 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-
-var fortuneCookies = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
+app.use(session({
+  secret: 'recommand 128 bytes random string', // 建议使用 128 个字符的随机字符串
+  cookie: { maxAge: 60 * 1000 }
+}));
 /*set up Routers*/
 
 router.get('/login', function(req, res) {
+	// req.session.isLogin=false;
+	console.log("session login state: "+req.session.isLogin);
     res.render('auth/login');  
+});
+router.post('/login', function(request, response) {
+	// request.session.isLogin=false;
+	console.log("session login state: "+request.session.isLogin);
+	// response.send(request.body); 
+
+
+	var data="";
+	var postData = JSON.stringify(request.body);
+	var options = {
+		  hostname: '218.244.147.240',
+		  port: 8080,
+		  path: '/login',
+		  method: 'POST',
+		  headers: {
+		    'Content-Type': 'application/json',
+		    'Content-Length': postData.length
+		  }};
+		// console.log(options);
+	  var req = http.request(options, function(res) {
+			  // console.log('STATUS: ' + res.statusCode);
+			  // console.log('HEADERS: ' + JSON.stringify(res.headers));
+			  res.setEncoding('utf8');
+			  res.on('data', function (chunk) {
+			    console.log('BODY: ' + chunk);
+			    data=chunk;
+			  });
+			  res.on('end', function() {
+			    console.log('No more data in response.');
+			    var json_data = JSON.parse(data)
+			    if (json_data["state"]==="successful") {
+			    	console.log('login success!');
+			    	request.session.isLogin=true;
+			    	console.log("session login state: "+request.session.isLogin);
+			  		response.redirect(301,'/admin');//login to Admin
+			    };
+			  });
+			});
+		  req.on('error', function(e) {
+			  console.log('problem with request: ' + e.message);
+			});
+
+			// write data to request body
+		req.write(postData);
+		req.end();
+	// response.redirect('/admin'); 
+    // var postData = JSON.stringify(request.body);
+    // console.log("empty token: "+postData);
+    // if (!postData.token) {
+    // 	console.log("empty token: "+postData);
+    // } else 
+    // {
+    // 	console.log("successful: "+postData);
+    // };  
 });
 app.post('/api/:method/:path',function(request,response) {
 	console.log("get you api endpoint!");
@@ -64,12 +117,18 @@ app.post('/api/:method/:path',function(request,response) {
 			  // console.log('HEADERS: ' + JSON.stringify(res.headers));
 			  res.setEncoding('utf8');
 			  res.on('data', function (chunk) {
-			    // console.log('BODY: ' + chunk);
+			    console.log('BODY: ' + chunk);
 			    data=chunk;
 			  });
 			  res.on('end', function() {
 			    console.log('No more data in response.');
 			  	response.send(data);//send data back to front end
+			    var json_data = JSON.parse(data)
+			    if (request.params.path==='login'&&json_data["state"]==="successful") {
+			    	console.log('login success!');
+			    	request.session.isLogin=true;
+			    	console.log("session login state: "+request.session.isLogin);
+			    };
 			  });
 			});
 		  req.on('error', function(e) {
@@ -92,7 +151,13 @@ app.get('/', function(req, res) {
 // 	res.render('home');
 // });
 app.get('/admin', function(req, res) {
-	res.render('admin/dashboard',{layout:'main_pure'});
+	console.log("session login state: "+req.session.isLogin);
+	if (req.session.isLogin==undefined||!req.session.isLogin) {
+		res.redirect(301,'/auth/login');
+	} 
+		else{
+			res.render('admin/dashboard',{layout:'main_pure'});
+		};
 });
 app.get('/about', function(req, res) {
 	var randomFortune =
