4822d0c96ce64aee02677e819e33b805011d8583
mzvast <mzvast@gmail.com>
2016-01-04 15:03:08 +0800

    Add ViewModel3&4 to decouple all the UI components. Reorganize code structure.

--- js/user/activity/manage.js
+++ js/user/activity/manage.js
@@ -20,49 +20,50 @@ $(document).ready(function() {
 		});
 	};
 	var shouter = new ko.subscribable();
-
+	//我发布的活动列表
 	var ViewModel1 = function() {
 		var self = this;
-		self.itemSizeOfActivityList = ko.observable(0);
-		self.itemSizeOfActivityList.subscribe(function(value) {
+		self.itemSize = ko.observable(0);
+		self.list = ko.observableArray();
+		self.currentPageIndex = ko.observable(1);
+		self.token = "027706ea56487ce6af2ab2b0e65268fc";
+		// Publish ActivityList itemSize to ViewModel2
+		self.itemSize.subscribe(function(value) {
 			shouter.notifySubscribers(value,"Publish_itemSizeOfActivityList");
 		});
-		self.activityList = ko.observableArray();
-		self.selectedActivity = ko.observable();
-		self.selectedActivity.subscribe(function(data) {
+		self.selectedItem = ko.observable();
+		// Publish selectedActivityItem to ViewModel3 
+		self.selectedItem.subscribe(function(data) {
 			shouter.notifySubscribers(data,"Publish_selectedActivity");
 		});
-
-		self.currentActivityListPageNum = ko.observable(1);
-		self.token = "027706ea56487ce6af2ab2b0e65268fc";
-
+		// Subscribe currentPageIndex change from ViewModel2 
 		shouter.subscribe(function(value) {
-			self.currentActivityListPageNum(value);
+			self.currentPageIndex(value);
 			self.fetchPublishedActivity();
-			console.log("self.currentActivityListPageNum()="+self.currentActivityListPageNum());
+			console.log("self.currentPageIndex()="+self.currentPageIndex());
 		},self,"Publish_ActivityListPageNum");
 
 		self.getSignupList =function(data) {
-			self.selectedActivity(data);
+			self.selectedItem(data);
 		};
 
 		self.writeActivityList=function(data) {
-			self.activityList([]);
+			self.list([]);
 			data.forEach(function(activityItem) {
-				self.activityList.push(new ActivityLite(activityItem));
+				self.list.push(new ActivityLite(activityItem));
 			});
 		};	
 
 
 		self.fetchPublishedActivity =function() {
-			console.log("正在获取已发布活动信息! page:"+self.currentActivityListPageNum());
+			console.log("正在获取已发布活动信息! page:"+self.currentPageIndex());
 			$.ajax({
 					  type: "POST",
 					  url: "/api/post/getpublishactivity",
 					  dataType: "json",
 					  data:{
 					  	"token": self.token,
-					  	"page": self.currentActivityListPageNum()
+					  	"page": self.currentPageIndex()
 					  },
 					})
 			.done(function(json) {
@@ -76,7 +77,8 @@ $(document).ready(function() {
 				       else{
 				       		console.dir("got data!");
 				       		self.writeActivityList(data);
-				       		self.selectedActivity(self.activityList()[0]);
+				       		self.selectedItem(self.list()[0]);
+				       		// self.itemSize(19);//TODO:set itemSize
 				       		console.dir(data);
 							return;
 				       };
@@ -97,41 +99,74 @@ $(document).ready(function() {
 	var ViewModel2 = function() {
 		var self = this;
 		self.itemSize = ko.observable();
-		self.pageNumbers = ko.observableArray();
-		self.currentActivityListPageNum = ko.observable(1);
-		self.currentActivityListPageNum.subscribe(function(value) {
+		self.pageNumbers = ko.observableArray([1]);
+		self.currentPageIndex = ko.observable(1);
+		self.lastPageIndex = ko.computed(function() {
+			return Math.ceil(self.itemSize()/10);
+		});
+		// Publish currentPageIndex to ViewModel1
+		self.currentPageIndex.subscribe(function(value) {
 			shouter.notifySubscribers(value,"Publish_ActivityListPageNum");
 		});
-
+		// Subscribe ActivityList itemSize
 		shouter.subscribe(function(value) {
 			self.itemSize(value);
-			console.log("VM2 self.itemSize()="+self.itemSize());
+			self.makePageNumbers(self.lastPageIndex());
+			console.log("VM2 self.lastPageIndex()="+self.lastPageIndex());
+			console.log("VM2 self.pageNumbers()="+self.pageNumbers());
 		},self,"Publish_itemSizeOfActivityList");
 
+		self.hasNext=ko.computed(function() {
+			console.log("VM2 self.currentPageIndex()="+self.currentPageIndex());
+			console.log("VM2 self.lastPageIndex()="+self.lastPageIndex());
+			return self.currentPageIndex()!==self.lastPageIndex();
+		});
+
+		self.hasPrecious=ko.computed(function() {
+			return self.currentPageIndex()>1;
+		});
+
+		self.makePageNumbers =function(lastPageIndex) {
+			self.pageNumbers.removeAll();
+			for (var i = 1; i <= lastPageIndex; i++) {
+				self.pageNumbers.push(i);
+			};
+		};
+
 		self.nextPage = function() {
-			self.currentActivityListPageNum(self.currentActivityListPageNum()+1);
-			console.log("self.currentActivityListPageNum()="+self.currentActivityListPageNum());
+			self.currentPageIndex(self.currentPageIndex()+1);
+			console.log("self.currentPageIndex()="+self.currentPageIndex());
 		};
 
 		self.previousPage = function() {
-			self.currentActivityListPageNum(self.currentActivityListPageNum()-1);
-			console.log("self.currentActivityListPageNum()="+self.currentActivityListPageNum());
+			self.currentPageIndex(self.currentPageIndex()-1);
+			console.log("self.currentPageIndex()="+self.currentPageIndex());
 		};
 
 	};
-
+	//报名用户列表
 	var ViewModel3 = function() {
 		var self = this;
 		self.itemSize = ko.observable(0);
+		// Publish SignupList itemSize to ViewModel4
+		self.itemSize.subscribe(function(value) {
+			shouter.notifySubscribers(value,"Publish_itemSizeOfSignupList");
+		});
 		self.token = "027706ea56487ce6af2ab2b0e65268fc";
 		self.list = ko.observableArray();
 		self.id = ko.observable(3);
-
+		self.currentPageIndex = ko.observable(1);
+		// Subscribe selectedActivityItem from ViewModel1
 		shouter.subscribe(function(data) {
 			self.id(data.id());
 			self.fetchList(self.id());
-		},self,"Publish_selectedActivity");
-		self.currentPageNum = ko.observable(1);
+		},self,"Publish_selectedActivity");		
+		// Subscribe currentPageIndex from ViewModel4
+		shouter.subscribe(function(value) {
+			self.currentPageIndex(value);
+			self.fetchList(self.id());
+		},self,"Publish_SignupListPageNum");
+
 
 		self.writeList=function(data) {
 			self.list([]);
@@ -140,16 +175,8 @@ $(document).ready(function() {
 			});
 		};
 
-		// self.getList = function(data) {
-		// 	console.log("activityid: "+data.id());
-		// 	self.fetchList(data.id);
-		// 	self.selectedActivity(data);
-		// 	console.dir(self.selectedActivity());
-		// 	return;
-		// };
-
 		self.fetchList =function(activityId) {
-			console.log("正在获取报名信息! page:"+self.currentPageNum());
+			console.log("正在获取报名信息! page:"+self.currentPageIndex());
 			$.ajax({
 					  type: "POST",
 					  url: "/api/post/getactivityattentuser",
@@ -157,7 +184,7 @@ $(document).ready(function() {
 					  data:{
 					  	"token": self.token,
 					  	"activityid":activityId,
-					  	"page": self.currentPageNum()
+					  	"page": self.currentPageIndex()
 					  },
 					})
 			.done(function(json) {
@@ -173,6 +200,7 @@ $(document).ready(function() {
 				       		console.dir("got data!");
 				       		self.writeList(data);
 				       		console.dir(data);
+				       		// self.itemSize(19);//TODO:set itemSize
 							return;
 				       };
 					})
@@ -187,11 +215,62 @@ $(document).ready(function() {
 		self.fetchList(self.id());
 	};
 
+	//报名用户的pignation
+	var ViewModel4 = function() {
+		var self = this;
+		self.itemSize = ko.observable();
+		self.pageNumbers = ko.observableArray([1]);
+		self.currentPageIndex = ko.observable(1);
+		self.lastPageIndex = ko.computed(function() {
+			return Math.ceil(self.itemSize()/10);
+		});
+		// Publish SignupList currentPageIndex to ViewModel3
+		self.currentPageIndex.subscribe(function(value) {
+			shouter.notifySubscribers(value,"Publish_SignupListPageNum");
+		});
+		// Subscribe SignupList itemSize from ViewModel3
+		shouter.subscribe(function(value) {
+			self.itemSize(value);
+			self.makePageNumbers(self.lastPageIndex());
+			console.log("VM4 self.lastPageIndex()="+self.lastPageIndex());
+			console.log("VM4 self.pageNumbers()="+self.pageNumbers());
+		},self,"Publish_itemSizeOfSignupList");
+
+		self.hasNext=ko.computed(function() {
+			console.log("VM4 self.currentPageIndex()="+self.currentPageIndex());
+			console.log("VM4 self.lastPageIndex()="+self.lastPageIndex());
+			return self.currentPageIndex()!==self.lastPageIndex();
+		});
+
+		self.hasPrecious=ko.computed(function() {
+			return self.currentPageIndex()>1;
+		});
+
+		self.makePageNumbers =function(lastPageIndex) {
+			self.pageNumbers.removeAll();
+			for (var i = 1; i <= lastPageIndex; i++) {
+				self.pageNumbers.push(i);
+			};
+		};
+
+		self.nextPage = function() {
+			self.currentPageIndex(self.currentPageIndex()+1);
+			console.log("self.currentPageIndex()="+self.currentPageIndex());
+		};
+
+		self.previousPage = function() {
+			self.currentPageIndex(self.currentPageIndex()-1);
+			console.log("self.currentPageIndex()="+self.currentPageIndex());
+		};
+
+	};
+
 	var masterVM = (function() {
 		var self = this;
 		self.ViewModel1 = new ViewModel1();
 		self.ViewModel2 = new ViewModel2();
 		self.ViewModel3 = new ViewModel3();
+		self.ViewModel4 = new ViewModel4();
 	})();
 
 	ko.applyBindings(masterVM);
