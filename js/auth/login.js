2c247e9c4bfac9b220832b60987ea81af31977d4
mzvast <mzvast@gmail.com>
2016-01-04 19:43:02 +0800

    Fix Login and Register auto binding username&password

--- js/auth/login.js
+++ js/auth/login.js
@@ -2,31 +2,12 @@ $(function() {
 
 	var ViewModel = function() {
 		var self = this;
-		// self.username = ko.observable("administrator");
-		// self.password = ko.observable("12345678");
-		self.username = ko.observable("mzvast");
-		self.password = ko.observable("32125222");
+		self.username = ko.observable();
+		self.password = ko.observable();
 		self.passhash = ko.computed(function() {
         	return md5(self.password());
     	});
 
-		// self.doLogin=function() {
-		// 	$.ajax({
-		// 		  type: "POST",
-		// 		  url: "/api/post/login",
-		// 		  dataType: "json",
-		// 		  data:{
-		// 		  	"username": ""+self.username(),
-		// 		  	"password": ""+self.passhash()
-		// 		  },
-		// 		  success: function(json) {
-		// 		       console.log("token: "+json['token']);
-		// 		    },
-		// 		  error: function(e) {
-		// 		       console.log(e);
-  //   				}
-		// 		});
-		// };
 	};
 
 	ko.applyBindings(new ViewModel())
--- js/auth/register.js
+++ js/auth/register.js
@@ -8,23 +8,6 @@ $(function() {
         	return md5(self.password());
     	});
 
-		// self.doLogin=function() {
-		// 	$.ajax({
-		// 		  type: "POST",
-		// 		  url: "/api/post/login",
-		// 		  dataType: "json",
-		// 		  data:{
-		// 		  	"username": ""+self.username(),
-		// 		  	"password": ""+self.passhash()
-		// 		  },
-		// 		  success: function(json) {
-		// 		       console.log("token: "+json['token']);
-		// 		    },
-		// 		  error: function(e) {
-		// 		       console.log(e);
-  //   				}
-		// 		});
-		// };
 	};
 
 	ko.applyBindings(new ViewModel())
